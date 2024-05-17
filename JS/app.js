0c497f6ad1614ceea19c353e50650f8b07f9a013
Cauê Vitor <138812443+cauevitorr@users.noreply.github.com>
2024-05-17 18:11:29 -0300

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,29 @@
+const urlBase02 = "https://api.github.com/users/cauevitorr"
+
+const loadMe = async () => {
+    const res = await fetch(urlBase02);
+    const data = await res.json();
+    return {results: data}
+};
+
+const loadMyPromisse = async () => {
+    const eu = await Promise.all(loadMe());
+    console.log('Me: ', eu.results);
+    showMe(eu.results);
+    // return
+};
+
+loadMe()
+
+//!----------------------------------------------------------------------------!//
+
 const urlBase = "https://api.github.com/users/cauevitorr/followers";
 
 const loadfollowers = async () => {
     const res = await fetch(urlBase);
     const data = await res.json();
-     const limitData = data.slice(0, 18);
-     return { results: limitData };
+    const limitData = data.slice(0, 18);
+    return { results: limitData };
 };
 
 const loadAllWithPromiseAll = async () => {
@@ -22,19 +41,19 @@ function showCharacter(followers) {
         divFollow.id = `Follow-${follower.id}`;
         divFollow.innerHTML = `
             <img src="${follower.avatar_url}" alt="Imagem do personagem">
-            <article class="character-info">
+            <article class="follower-info">
                 <h2>${follower.login}</h2>
             </article>
         `;
-        divFollow.classList.add('character-box');
+        divFollow.classList.add('follower-box');
         followersContainer.appendChild(divFollow);
         divFollow.addEventListener('click', () => {
-            characterDetails(follower.id);
+            followerDetails(follower.id);
         });
     });
 }
 
-function characterDetails(id) {
+function followerDetails(id) {
     console.log(id);
     const idEncrypted = encryptId(id);
     console.log(idEncrypted);
@@ -44,12 +63,3 @@ function characterDetails(id) {
 function encryptId(id) {
     return id;
 }
-
-//                 <span class="${character.alive}">${character.alive === true ? 'Alive' : 'Dead'}</span>
-//<span class="${character.species}">Specie - ${character.species}</span>
-//<span class="${character.gender}">Gender - ${character.gender}</span>
-//<span class="${character.patronus}">Patronus - ${character.patronus === "" ? 'unknow' : character.patronus}</span>
-//<span class="${character.house}">House - ${character.house === "" ? 'unknow' : character.house}</span>
-//<span class="${character.yearOfBirth}">Year Of Birth - ${character.yearOfBirth === null ? 'unknow' : character.yearOfBirth}</span>
-//<span class="${character.ancestry}">Ancestry - ${character.ancestry}</span>
-//<span class="${character.actor}">Actor - ${character.actor}</span>
